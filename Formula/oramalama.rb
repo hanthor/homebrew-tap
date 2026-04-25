70804c99ac77d3944399b159ee5c8a1f223f8035
James Reilly <jreilly1821@gmail.com>
2026-04-25 10:35:52 +0530

    chore: bump oramalama to v0.1.1
    Updated formula with v0.1.1 binary
    - Fixed pi models.json schema (models must be array)
    - SHA256: 378a2c44f8953e0ed636500a94702d007a1455dc6e0902d1aeb1fe7228b00899

--- Formula/oramalama.rb
+++ Formula/oramalama.rb
@@ -2,27 +2,27 @@ class Oramalama < Formula
   desc "Local AI coding assistant powered by RamaLama"
   homepage "https://github.com/hanthor/oramalama"
   license "Apache-2.0"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/hanthor/oramalama/releases/download/v0.1.0/oramalama_0.1.0_darwin_arm64.tar.gz"
-      sha256 "135577bee79adf570107f1c83d614cc7325bd60e4b71921835ea7826a97b0b4d"
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.1/oramalama_0.1.1_darwin_arm64.tar.gz"
+      sha256 "378a2c44f8953e0ed636500a94702d007a1455dc6e0902d1aeb1fe7228b00899"
     end
     on_intel do
-      url "https://github.com/hanthor/oramalama/releases/download/v0.1.0/oramalama_0.1.0_darwin_amd64.tar.gz"
-      sha256 "135577bee79adf570107f1c83d614cc7325bd60e4b71921835ea7826a97b0b4d"
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.1/oramalama_0.1.1_darwin_amd64.tar.gz"
+      sha256 "378a2c44f8953e0ed636500a94702d007a1455dc6e0902d1aeb1fe7228b00899"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/hanthor/oramalama/releases/download/v0.1.0/oramalama_0.1.0_linux_arm64.tar.gz"
-      sha256 "135577bee79adf570107f1c83d614cc7325bd60e4b71921835ea7826a97b0b4d"
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.1/oramalama_0.1.1_linux_arm64.tar.gz"
+      sha256 "378a2c44f8953e0ed636500a94702d007a1455dc6e0902d1aeb1fe7228b00899"
     end
     on_intel do
-      url "https://github.com/hanthor/oramalama/releases/download/v0.1.0/oramalama_0.1.0_linux_amd64.tar.gz"
-      sha256 "135577bee79adf570107f1c83d614cc7325bd60e4b71921835ea7826a97b0b4d"
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.1/oramalama_0.1.1_linux_amd64.tar.gz"
+      sha256 "378a2c44f8953e0ed636500a94702d007a1455dc6e0902d1aeb1fe7228b00899"
     end
   end
 

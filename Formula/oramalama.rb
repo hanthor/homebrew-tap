41a710fe88563a3a20052b1e3685b5b3a1b52c32
James Reilly <jreilly1821@gmail.com>
2026-04-25 10:42:54 +0530

    chore: bump oramalama to v0.1.2
    Fixed pi provider configuration with api field
    SHA256: 77e42ef9f586f9fc105a30215ed8642ebbf2690fb38952e46c05ee4d8fa30f42

--- Formula/oramalama.rb
+++ Formula/oramalama.rb
@@ -2,27 +2,27 @@ class Oramalama < Formula
   desc "Local AI coding assistant powered by RamaLama"
   homepage "https://github.com/hanthor/oramalama"
   license "Apache-2.0"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/hanthor/oramalama/releases/download/v0.1.1/oramalama_0.1.1_darwin_arm64.tar.gz"
-      sha256 "378a2c44f8953e0ed636500a94702d007a1455dc6e0902d1aeb1fe7228b00899"
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.2/oramalama_0.1.2_darwin_arm64.tar.gz"
+      sha256 "77e42ef9f586f9fc105a30215ed8642ebbf2690fb38952e46c05ee4d8fa30f42"
     end
     on_intel do
-      url "https://github.com/hanthor/oramalama/releases/download/v0.1.1/oramalama_0.1.1_darwin_amd64.tar.gz"
-      sha256 "378a2c44f8953e0ed636500a94702d007a1455dc6e0902d1aeb1fe7228b00899"
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.2/oramalama_0.1.2_darwin_amd64.tar.gz"
+      sha256 "77e42ef9f586f9fc105a30215ed8642ebbf2690fb38952e46c05ee4d8fa30f42"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/hanthor/oramalama/releases/download/v0.1.1/oramalama_0.1.1_linux_arm64.tar.gz"
-      sha256 "378a2c44f8953e0ed636500a94702d007a1455dc6e0902d1aeb1fe7228b00899"
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.2/oramalama_0.1.2_linux_arm64.tar.gz"
+      sha256 "77e42ef9f586f9fc105a30215ed8642ebbf2690fb38952e46c05ee4d8fa30f42"
     end
     on_intel do
-      url "https://github.com/hanthor/oramalama/releases/download/v0.1.1/oramalama_0.1.1_linux_amd64.tar.gz"
-      sha256 "378a2c44f8953e0ed636500a94702d007a1455dc6e0902d1aeb1fe7228b00899"
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.2/oramalama_0.1.2_linux_amd64.tar.gz"
+      sha256 "77e42ef9f586f9fc105a30215ed8642ebbf2690fb38952e46c05ee4d8fa30f42"
     end
   end
 

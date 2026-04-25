5b95f18d1b45930fa6a15d40f14c48f8bbdfe50d
James Reilly <jreilly1821@gmail.com>
2026-04-25 10:27:32 +0530

    chore: add oramalama formula for v0.1.0
    SHA256: 135577bee79adf570107f1c83d614cc7325bd60e4b71921835ea7826a97b0b4d

--- Formula/oramalama.rb
+++ Formula/oramalama.rb
@@ -1,10 +1,30 @@
 class Oramalama < Formula
   desc "Local AI coding assistant powered by RamaLama"
   homepage "https://github.com/hanthor/oramalama"
-  url "https://github.com/hanthor/oramalama/releases/download/#{version}/oramalama_#{version}_#{os}_#{hardware_arch}.tar.gz"
-  version "{{VERSION}}"
-  sha256 "{{SHA256}}"
   license "Apache-2.0"
+  version "0.1.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.0/oramalama_0.1.0_darwin_arm64.tar.gz"
+      sha256 "135577bee79adf570107f1c83d614cc7325bd60e4b71921835ea7826a97b0b4d"
+    end
+    on_intel do
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.0/oramalama_0.1.0_darwin_amd64.tar.gz"
+      sha256 "135577bee79adf570107f1c83d614cc7325bd60e4b71921835ea7826a97b0b4d"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.0/oramalama_0.1.0_linux_arm64.tar.gz"
+      sha256 "135577bee79adf570107f1c83d614cc7325bd60e4b71921835ea7826a97b0b4d"
+    end
+    on_intel do
+      url "https://github.com/hanthor/oramalama/releases/download/v0.1.0/oramalama_0.1.0_linux_amd64.tar.gz"
+      sha256 "135577bee79adf570107f1c83d614cc7325bd60e4b71921835ea7826a97b0b4d"
+    end
+  end
 
   depends_on "ramalama"
   depends_on "gum"
@@ -15,16 +35,6 @@ class Oramalama < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/oramalama --version")
-  end
-
-  private
-
-  def os
-    OS.mac? ? "darwin" : "linux"
-  end
-
-  def hardware_arch
-    Hardware::CPU.arm? ? "arm64" : "amd64"
+    assert_match /^oramalama version/, shell_output("#{bin}/oramalama -version")
   end
 end

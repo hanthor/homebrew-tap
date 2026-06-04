3e941dd952aa21b54876581ca89f8cc7be0dc5f0
James Reilly <jreilly1821@gmail.com>
2026-06-04 04:32:31 +0000

    goose-linux 1.37.0

--- Casks/goose-linux.rb
+++ Casks/goose-linux.rb
@@ -1,6 +1,6 @@
 cask "goose-linux" do
-  version "1.34.1"
-  sha256 "04938dd8d84f04e2a51c09dfd4a9396b473c870fb60e6cbf0c458db5c6111dff"
+  version "1.37.0"
+  sha256 "829341de94b1b86e568d33558d522d5ca547f930a5fb473747110641de7dff51"
 
   url "https://github.com/block/goose/releases/download/v#{version}/Goose-#{version}-1.x86_64.rpm",
       verified: "github.com/block/goose/"

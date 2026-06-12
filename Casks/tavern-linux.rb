8cfdd5bd46c94b1c697b6d3adb647fe0467420bd
James Reilly <jreilly1821@gmail.com>
2026-06-12 04:40:02 +0000

    tavern-linux 0.1.5

--- Casks/tavern-linux.rb
+++ Casks/tavern-linux.rb
@@ -1,9 +1,8 @@
 cask "tavern-linux" do
-  version "0.1.3"
-  sha256 "4712464d96f67bd75f34f022f5f1761bcd3e6cdabd17dc8c870bd41b4ca6f349"
+  version "0.1.5"
+  sha256 "b0c4b626fa9d75a249843b194d1d6129ad3b0bc11e3f2f4e947b6d705ba8e37c"
 
   url "https://github.com/hanthor/Tavern/releases/download/v#{version}/Tavern-Linux.flatpak"
-  container type: :naked
   name "Tavern"
   desc "Modern Homebrew client for Linux built with Python and GTK 4"
   homepage "https://github.com/hanthor/Tavern"
@@ -13,18 +12,20 @@ cask "tavern-linux" do
     strategy :github_latest
   end
 
+  container type: :naked
+
   postflight do
     system_command "flatpak",
-      args: ["install", "--user", "--noninteractive", staged_path/"Tavern-Linux.flatpak"],
-      sudo: false
+                   args: ["install", "--user", "--noninteractive", staged_path/"Tavern-Linux.flatpak"],
+                   sudo: false
   end
 
   uninstall_postflight do
     system_command "flatpak",
-      args: ["remove", "--user", "--noninteractive", "dev.hanthor.Tavern"],
-      print_stderr: false,
-      sudo: false
-  rescue StandardError
+                   args:         ["remove", "--user", "--noninteractive", "dev.hanthor.Tavern"],
+                   print_stderr: false,
+                   sudo:         false
+  rescue
     nil
   end
 
